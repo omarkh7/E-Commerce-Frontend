1889f6b4cf2643f7b95de40464f24337b63eaeb2
Youssef <youssefhamadi94@gmail.com>
2023-04-17 16:58:54 +0300

    wwa

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
 import Navhead from './components/Navhead/Navhead'
-import Home from './components/Home Page/Home'; 
+import Home from './components/Home Page/Home';
 import About from './components/About Me/About';
 import Contact from './components/Contact us /Contact';
 import Shop from './components/Shop/Shop';
+import ContactForm from './components/Contact us /ContactForm';
 
 
 function App() {
@@ -13,15 +14,16 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navhead />
-   
-       
+        <Contact />
+        <ContactForm />
+
         <Routes>
           <Route path='/Home' element={<Home />} />
-          <Route path='/Home' element= {<Home />} />
-          <Route path='/about' element ={<About />} />
-          <Route path='/contact' element= {<Contact />} />
-          <Route path='/login' element= {<Login />} />
-          <Route path='/shop' element ={<Shop />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/shop' element={<Shop />} />
           <Route path='/terms' element={<Home />} />
           <Route path='/refund' element={<Home />} />
         </Routes>
