642ce1af18e2be3ac947aa7d687ba6cfd57239ce
Youssef <youssefhamadi94@gmail.com>
2023-04-17 17:06:16 +0300

    qw

--- src/components/Contact us /Contact.jsx
+++ src/components/Contact us /Contact.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import './Contact.css'
+// import emailjs from '@emailjs/browser';
 
-function Contact() {
+
+
+const Contact = () => {
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, e.target, process.env.REACT_APP_PUBLIC_KEY);
+  // }
+
+
+
   return (
-    <div>
-      
+
+    <div className='contact_div1'>
+      <img src='https://r4.wallpaperflare.com/wallpaper/713/298/326/air-force-ked-nike-wallpaper-82cb2aa572de89020d0668a66156dd6b.jpg' />
+      <h1 className='contact_h1'>
+        Are you interested to know more about us and about our products?
+      </h1>
+      <h2 className='contact_h2'>
+        Contact Us
+      </h2>
+
     </div>
+
   )
 }
 
