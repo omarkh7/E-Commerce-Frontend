2541c84311c5386e6f97fed5ba28466542545e4e
Youssef <youssefhamadi94@gmail.com>
2023-04-18 15:17:42 +0300

    wer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
 import Navhead from './components/Navhead/Navhead'
-import Home from './components/Home Page/Home'; 
+import Home from './components/Home Page/Home';
 import About from './components/About Me/About';
 import Contact from './components/Contact us /Contact';
 import Shop from './components/Shop/Shop';
@@ -13,15 +13,14 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navhead />
-   
-       
+
         <Routes>
           <Route path='/Home' element={<Home />} />
-          <Route path='/Home' element= {<Home />} />
-          <Route path='/about' element ={<About />} />
-          <Route path='/contact' element= {<Contact />} />
-          <Route path='/login' element= {<Login />} />
-          <Route path='/shop' element ={<Shop />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/shop' element={<Shop />} />
           <Route path='/terms' element={<Home />} />
           <Route path='/refund' element={<Home />} />
         </Routes>
