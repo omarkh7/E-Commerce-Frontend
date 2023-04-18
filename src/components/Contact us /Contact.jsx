16ce77e96cac346b9044cbd158e8c13ea7613fbd
Youssef <youssefhamadi94@gmail.com>
2023-04-18 14:02:15 +0300

    Contact Page

--- src/components/Contact us /Contact.jsx
+++ src/components/Contact us /Contact.jsx
@@ -14,18 +14,20 @@ const Contact = () => {
 
 
   return (
+    <>
+      {/* <div className='contact_div1'>
+        <img src='https://r4.wallpaperflare.com/wallpaper/713/298/326/air-force-ked-nike-wallpaper-82cb2aa572de89020d0668a66156dd6b.jpg' />
 
-    <div className='contact_div1'>
-      <img src='https://r4.wallpaperflare.com/wallpaper/713/298/326/air-force-ked-nike-wallpaper-82cb2aa572de89020d0668a66156dd6b.jpg' />
+      </div>
       <h1 className='contact_h1'>
-        Are you interested to know more about us and about our products?
+        Are You Interested To Know More About Us And Our Products?
       </h1>
+
       <h2 className='contact_h2'>
         Contact Us
-      </h2>
+      </h2> */}
 
-    </div>
-
+    </>
   )
 }
 
