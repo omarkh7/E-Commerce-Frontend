65e9341f15806d154f1f3eb14a15ff3822c763d9
Omar Khankan <omar.khankan@gmail.com>
2023-04-19 21:50:54 +0300

    getting categories

--- src/components/Home Page/Home.jsx
+++ src/components/Home Page/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Categories from './Categories/Categories'
 import Slider from './Slider/Slider'
 
 function Home() {
   return (
     <div>
       <Slider/>
+      <Categories/>
     </div>
   )
 }
