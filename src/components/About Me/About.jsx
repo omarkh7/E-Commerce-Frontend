46b96c1fb4ca6f849f90ba7a642b0dbd610db0d0
Youssef <youssefhamadi94@gmail.com>
2023-04-19 08:15:20 +0300

    aa

--- src/components/About Me/About.jsx
+++ src/components/About Me/About.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import './About.css';
+
+
+
 
 function About() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+      <div className='box'>
+        <h1 className='h1_walkingexperience'>We Provide You With The Best Walking Experience.
+        </h1>
+      </div>
+      <hr />
+    </>
+
   )
 }
 
