583e2f4bb1be642ebe2269ea1ecc2972be17aba4
Omar Khankan <omar.khankan@gmail.com>
2023-04-19 16:28:51 +0300

    Adding Slider

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
    
        
         <Routes>
-          <Route path='/Home' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/Home' element= {<Home />} />
           <Route path='/about' element ={<About />} />
           <Route path='/contact' element= {<Contact />} />
