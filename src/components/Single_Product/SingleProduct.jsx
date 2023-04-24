229d8246d2a6c2a847fb1dce0ae9b5e3b876181d
Omar Khankan <omar.khankan@gmail.com>
2023-04-24 12:25:50 +0300

    single

--- src/components/Single_Product/SingleProduct.jsx
+++ src/components/Single_Product/SingleProduct.jsx
@@ -1,3 +1,46 @@
+// import { useParams } from "react-router-dom";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// function SingleProduct() {
+//   const { productId } = useParams();
+
+//   const [alldata, setAllData] = useState([]);
+
+//   const apiURL = `http://localhost:8000/api/products/getproductbyid/${productId}`;
+
+//   const fetchallData = async () => {
+//     try {
+//       const response = await axios.get(apiURL);
+//       console.log("hello:", response.data.product);
+//       setAllData(response.data.product);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchallData();
+//   }, []);
+//   return (
+//     <div>
+//       {alldata ? (
+//         alldata.map((item) => (
+//           <div key={item._id}>
+//             <h2>{item.name}</h2>
+//             <h2>{item.description}</h2>
+//             <img src={item.image} alt={item.name} />
+//           </div>
+//         ))
+//       ) : (
+//         <p>No data available</p>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default SingleProduct;
+
 import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
@@ -5,7 +48,7 @@ import axios from "axios";
 function SingleProduct() {
   const { productId } = useParams();
 
-  const [alldata, setAllData] = useState([]);
+  const [alldata, setAllData] = useState(null);
 
   const apiURL = `http://localhost:8000/api/products/getproductbyid/${productId}`;
 
@@ -13,7 +56,7 @@ function SingleProduct() {
     try {
       const response = await axios.get(apiURL);
       console.log("hello:", response.data.product);
-      setAllData(response.data.product);
+      setAllData([response.data.product]);
     } catch (error) {
       console.error(error);
     }
@@ -22,10 +65,10 @@ function SingleProduct() {
   useEffect(() => {
     fetchallData();
   }, []);
-
+  
   return (
     <div>
-      {alldata.length > 0 ? (
+      {alldata ? (
         alldata.map((item) => (
           <div key={item._id}>
             <h2>{item.name}</h2>
@@ -41,3 +84,4 @@ function SingleProduct() {
 }
 
 export default SingleProduct;
+
