d8e42a459003d01200986399fa6ce135ddde9ebb
Maya <maya.atiah.99@outlook.com>
2023-04-26 14:54:21 +0300

    solving conflicts

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import Navhead from "./components/Navhead/Navhead";;
 import Home from "./components/Home Page/Home";
 import About from "./components/About Me/About";
 import Contact from "./components/Contact us /Contact";
-import Hightop from "./components/Shop/Categories/Hightop/Hightop";
-import Lowtop from "./components/Shop/Categories/Lowtop/Lowtop";
-import Athletic from "./components/Shop/Categories/Athletic/Athletic"
-import Retro from "./components/Shop/Categories/Retro/Retro"
-import Slide from "./components/Shop/Categories/Slides/Slides"
-import Socks from "./components/Shop/Categories/Socks/Socks"import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer';
 import SingleProduct from './components/Single_Product/SingleProduct';
 import SingleCategory from './components/Single_Category/SingleCategory';
 
@@ -22,7 +17,6 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
@@ -30,24 +24,13 @@ function App() {
           <Route path="/terms" element={<Home />} />
           <Route path="/refund" element={<Home />} />
           <Route path="/hightop" element={<Hightop />} />
+          <Route path='/shop' element={<Shop />} />
 
-          <Route path="/lowtop" element={<Lowtop/>} />
-          <Route path="/athletic" element={<Athletic />} />
-          <Route path="/retro" element={<Retro/>} />
-          <Route path="/slides" element={<Slide />} />
-          <Route path="/socks" element={<Socks/>} />
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element ={<About />} />
-          <Route path='/contact' element= {<Contact />} />
-          <Route path='/login' element= {<Login />} />
-          <Route path='/shop' element ={<Shop />} />
-          <Route path='/terms' element={<Home />} />
-          <Route path='/refund' element={<Home />} />
-          <Route path='/single-product/:productId' element={<SingleProduct/>} />
-          <Route path='/single-category/:categoryId' element={<SingleCategory/>} />
-          
+          <Route path='/single-product/:productId' element={<SingleProduct />} />
+          <Route path='/single-category/:categoryId' element={<SingleCategory />} />
+
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </BrowserRouter>
   );
