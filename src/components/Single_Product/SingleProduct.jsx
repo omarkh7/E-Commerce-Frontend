e6f2f71619cb2475e1773b088d93c69ba8d1c86a
Omar Khankan <omar.khankan@gmail.com>
2023-04-29 15:36:05 +0300

    single

--- src/components/Single_Product/SingleProduct.jsx
+++ src/components/Single_Product/SingleProduct.jsx
@@ -7,24 +7,6 @@ function SingleProduct() {
   const { productId } = useParams();
 
   const [alldata, setAllData] = useState(null);
-  // const [selectedSize, setSelectedSize] = useState('');
-  // const [selectedColor, setSelectedColor] = useState('');
-  // const [selectedQuantity, setSelectedQuantity] = useState(1);
-
-  // const handleSizeChange = (event) => {
-  //   setSelectedSize(event.target.value);
-  // };
-
-  // const handleColorChange = (event) => {
-  //   setSelectedColor(event.target.value);
-  // };
-
-  // const handleQuantityChange = (event) => {
-  //   setSelectedQuantity(parseInt(event.target.value));
-  // };
-
-  // const sizeOptions = alldata.attribute.map((attr) => attr.size);
-  // const colorOptions = alldata.attribute.map((attr) => attr.color);
 
   const apiURL = `http://localhost:8000/api/products/getproductbyid/${productId}`;
 
@@ -42,33 +24,12 @@ function SingleProduct() {
     fetchallData();
   }, []);
 
-  // return (
-  //   <div>
-  //     {alldata ? (
-  //       alldata.map((item) => (
-  //         <div key={item._id}>
-  //           <h2>{item.name}</h2>
-  //           <h2>{item.description}</h2>
-  //           <h2>{item.price} $</h2>
-  //           <h2>{item.category.name} </h2>
-  //           <h2>{item.countInStock} </h2>
-
-  //           <img src={item.image} alt={item.name} />
-  //           <img src={item.images[1]}/>
-  //           <img src={item.images[2]}/>
-  //         </div>
-  //       ))
-  //     ) : (
-  //       <p>No data available</p>
-  //     )}
-  //   </div>
-  // );
   return (
     <div className="product-details-container">
       {alldata ? (
         alldata.map((product) => (
           <div key={product._id} className="product-details">
-            <div class="image-wrapper">
+            <div className="image-wrapper">
               <ul>
                 {product.images &&
                   product.images.map((image, index) => (
@@ -82,20 +43,20 @@ function SingleProduct() {
               <h1 className="product-name">{product.name}</h1>
               <p className="product-description">{product.description}</p>
               <h2 className="product-price">{product.price} $</h2>
-              <div className="product-attributes">
-                {product.attribute &&
-                  product.attribute.map((attr, index) => (
-                    <div key={index} className="product-attribute">
-                      <h3 className="attribute-title">Size:</h3>
-                      <p className="attribute-value">{attr.size}</p>
-                      <h3 className="attribute-title">Color:</h3>
-                      <p className="attribute-value">{attr.color}</p>
-                      <h3 className="attribute-title">Quantity:</h3>
-                      <p className="attribute-value">{attr.quantity}</p>
-                    </div>
-                  ))}
-              </div>
+              {product.attribute && (
+                <select>
+                  {product.attribute &&
+                    (Array.isArray(product.attribute.size)
+                      ? product.attribute.size.map((size) => (
 
+                          <option value={size} key={size}>
+                            {size` ${console.log("SIZE",size)}`} Size
+                     
+                          </option>
+                        ))
+                      : null)}
+                </select>
+              )}
               <div className="product-actions">
                 <button className="add-to-cart-button">Add to Cart</button>
               </div>
