ab9d258e5469f62a3fa939504c5c3802ef53c055
Maya <maya.atiah.99@outlook.com>
2023-04-29 14:09:44 +0300

    edditing the about page

--- src/components/About Me/About.jsx
+++ src/components/About Me/About.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import { useEffect } from "react";
 import "./About.css";
 import axios from "axios";
-
+import Logo from "../../assets/images/logoAbout.png";
+import {
+  BsFillChatLeftQuoteFill,
+  BsFillChatRightQuoteFill,
+} from "react-icons/bs";
+import { VscWorkspaceTrusted } from "react-icons/vsc";
+import { FaExclamation } from "react-icons/fa";
 function About() {
   useEffect(() => {
     getabout();
@@ -15,35 +21,33 @@ function About() {
     const response = await axios.get(
       `http://localhost:8000/api/pages/getallpagesbyid/6440f68433ababf0bb2ad176`
     );
-
-    console.log("data ", response.data.data);
-
     setaboutdata(response.data.data.description);
     setaboutimage(response.data.data.image);
   };
 
-  /* <img alt='img' placehoder="IMAGE" src={`http://localhost:8000/images/${aboutimage}`} /> */
 
   return (
     <>
-      <img
-        src='https://c0.wallpaperflare.com/preview/186/124/897/red-and-white-air-jordan-1-shoe-on-concrete-floor.jpg'
-        alt='img'
-      />
-      <div id='box'>
-        <h1 className='h1_walkingexperience'>
-          We Provide You With The Best Walking Experience!
-        </h1>
+      <div className='hero-image-about'></div>
+
+      <div className='content-about'>
+        {" "}
+        <img src={Logo} alt='logo' className='logo-about' />
+        <div className='description-about'>
+          <div className='quote-titile-about'>
+            <BsFillChatLeftQuoteFill className='quote-icon-about' />{" "}
+            <p>We provide you with the better walking experience</p>
+            <BsFillChatRightQuoteFill className='quote-icon-about' />
+          </div>
+          <div className='about-data'>{aboutdata}</div>
+          <div className='quote-titile-about'>
+            {" "}
+            <VscWorkspaceTrusted className='quote-icon-about' />
+            <p>Thank you for choosing our website </p>{" "}
+            <FaExclamation className='quote-icon-about' />
+          </div>
+        </div>
       </div>
-      <div className='div_h3'>
-        <h3 className='h3_aboutUs'>
-          {aboutdata}
-          {console.log("img ", `localhost/8000/images/${aboutimage}`)}
-        </h3>
-      </div>
-      <span className='span_aboutUs'>
-        <h5>Thank you for choosing our website!</h5>
-      </span>
     </>
   );
 }
